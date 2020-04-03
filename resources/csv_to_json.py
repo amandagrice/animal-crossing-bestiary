1c0ac03011176ae69ff18e322227f2c8901bfcaa
Amanda Grice <algrice@bu.edu>
2020-04-02 20:41:05 -0400

    add bugs/fish resources

---
+++ resources/csv_to_json.py
@@ -0,0 +1,52 @@
+import csv
+import json
+
+
+def get_months(months):
+	if (months.strip() == "All"):
+		months = list(range(1, 13))
+	else:
+		months = months.split(';')
+		for i in range(len(months)):
+			months[i] = int(months[i])
+	return months
+
+bugs = []
+with open('bugs.csv', newline='\n') as bug_csvfile:
+	bugreader = csv.reader(bug_csvfile, delimiter=',', quotechar='|')
+	for row in bugreader:
+		bugs.append(
+			{
+				"id": int(row[0]),
+				"name": row[1],
+				"location": row[2],
+				"value": int(row[3]),
+				"months": get_months(row[4]),
+				"times": row[5].split('/')
+
+			})
+print(json.dumps(bugs, indent=4))
+
+with open('bugs.json', 'w') as bug_outfile:
+    json.dump(bugs, bug_outfile, indent=4)
+
+
+fish = []
+with open('fish.csv', newline='\n') as fish_csvfile:
+	fishreader = csv.reader(fish_csvfile, delimiter=',', quotechar='|')
+	for row in fishreader:
+		fish.append(
+			{
+				"id": int(row[0]),
+				"name": row[1],
+				"location": row[2],
+				"value": int(row[3]),
+				"months": get_months(row[4]),
+				"times": row[5].split('/'),
+				"shadow": row[6]
+
+			})
+print(json.dumps(fish, indent=4))
+
+with open('fish.json', 'w') as fish_outfile:
+    json.dump(fish, fish_outfile, indent=4)

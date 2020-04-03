1e5369014d20f52796a37dc4d09c0fbe48fd60e3
Amanda Grice <algrice@bu.edu>
2020-04-02 20:41:56 -0400

    clean

--- backend/critterpedia/src/main/scala/com/grice/critterpedia/Main.scala
+++
@@ -1,9 +0,0 @@
-package com.grice.critterpedia
-
-import cats.effect.{ExitCode, IO, IOApp}
-import cats.implicits._
-
-object Main extends IOApp {
-  def run(args: List[String]) =
-    CritterpediaServer.stream[IO].compile.drain.as(ExitCode.Success)
-}

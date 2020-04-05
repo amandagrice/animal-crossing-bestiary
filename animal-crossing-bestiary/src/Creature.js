d91d4cf48571a636276732223ebd04cb0f438037
Amanda Grice <algrice@bu.edu>
2020-04-05 01:02:50 -0400

    add initial images

--- animal-crossing-bestiary/src/Creature.js
+++ animal-crossing-bestiary/src/Creature.js
@@ -4,7 +4,7 @@ function Creature(props) {
     return(
         <div style={{backgroundColor: 'lightgray', height: '250px', width: '250px', marginLeft: '50px', marginBottom: '50px', marginTop: '50px'}}>
             <div className="creature-name" style={{fontSize: '30px', textAlign: 'center'}}>{props.data.name}</div>
-            <img src={props.data.name + '.png'} style={{width: '250px'}} />
+            <img src={'./fish/' + props.data.name.replace(/ /g, '_') + '.png'} style={{maxWidth: '250px', maxHeight: '200px'}} />
             <br />
         </div>
     )

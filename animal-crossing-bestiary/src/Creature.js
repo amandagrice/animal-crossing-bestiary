6ce773d24c5b848e08884557a45a73a4a2a6a1d5
Amanda Grice <algrice@bu.edu>
2020-04-06 22:39:30 -0400

    update images to be transparent

--- animal-crossing-bestiary/src/Creature.js
+++ animal-crossing-bestiary/src/Creature.js
@@ -6,7 +6,9 @@ function Creature(props) {
         <div style={{backgroundColor: '#dedede', height: '300px', width: '300px', marginLeft: '100px', marginBottom: '30px', marginTop: '30px', position: 'relative', borderRadius: '10%', border: '2px solid black'}}>
             <div style={{position: 'absolute', top: 10, left: 15}}>No. {props.data.id}</div>
             <div className="creature-name" style={{fontSize: '25px', textAlign: 'right', marginRight: '15px'}}>{props.data.name}</div>
-            <img src={'./fish/' + props.data.name.replace(/ /g, '_') + '.png'} style={{maxWidth: '250px', maxHeight: '200px'}} alt='' />
+            <div className={"fish-image-holder"} style={{width: '100%', height: '100px'}}>
+                <img className='fish-image' src={'./fish/' + props.data.name.replace(/ /g, '_') + '.png'} style={{maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }} alt='' />
+            </div>
             <div style={{fontSize: '17px', position: 'absolute', bottom: 10, left: 10}}>
                 <strong>Months:</strong> placeholder <br />
                 <strong>Times:</strong> placeholder <br />
